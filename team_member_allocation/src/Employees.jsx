16c9150e8eea81a60bdc1da36d89ac4d02de87aa
suheyl-yazici <h.suheylyazici@gmail.com>
2022-10-04 20:18:59 +0300

    Dosya formatları hazırlandı.Dk 23

---
+++ team_member_allocation/src/Employees.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Employees = () => {
+  return (
+    <main><h1>Employees</h1></main>
+  )
+}
+
+export default Employees
---
+++ team_member_allocation/src/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Header = () => {
+  return <header>
+    <h1>Header</h1></header>;
+};
+
+export default Header;
