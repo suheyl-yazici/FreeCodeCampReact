a4202b371307513a3605bb0f2bb28d778d1d9346
suheyl-yazici <h.suheylyazici@gmail.com>
2022-10-04 22:04:15 +0300

    props konusuna kadar gelindi. Dk 57

--- team_member_allocation/src/Content.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Content = () => {
-  return (
-    <main>Content</main>
-  )
-}
-
-export default Content
--- team_member_allocation/src/Employees.jsx
+++ team_member_allocation/src/Employees.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import femaleProfile from "./images/femaleProfile.jpg";
+import maleProfile from "./images/maleProfile.jpg";
 
 const Employees = () => {
-  return (
-    <main><h1>Employees</h1></main>
-  )
-}
 
-export default Employees
+  return (
+    <main className="container">
+      <div class="row justify-content-center mt-3 mb-3">
+        <div class="col-8">
+          <select
+            className="form-select form-select-lg"
+            value={selectedTeam}
+            onChange={handleTeamSelectionChange}
+          >
+            <option value="TeamA">TeamA</option>
+            <option value="TeamB">TeamB</option>
+            <option value="TeamC">TeamC</option>
+            <option value="TeamD">TeamD</option>
+          </select>
+        </div>
+      </div>
+      <div class="row justify-content-center mt-3 mb-3">
+        <div class="col-8">
+          <div class="card-collection">
+            {employees.map((employee) => (
+              <div
+                id={employee.id}
+                className={(employee.teamName === selectedTeam ? 'card m-2 standout' : 'card m-2')}
+                style={{ cursor: "pointer" }}
+                onClick={handleEmployeeCardClick}
+              >
+                {employee.gender === "male" ? (
+                  <img src={maleProfile} alt="" className="card-img-top" />
+                ) : (
+                  <img src={femaleProfile} alt="" className="card-img-top" />
+                )}
+
+                <div className="card-body">
+                  <h5 className="card-title">Full Name: {employee.fullName}</h5>
+                  <p className="card-text">
+                    <b>Designation:</b>
+                    {employee.designation}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Employees;
