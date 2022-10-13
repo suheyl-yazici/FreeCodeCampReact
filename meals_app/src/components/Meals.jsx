a78f2543b9dd8a73357448f19219a98c526d96bc
suheyl-yazici <h.suheylyazici@gmail.com>
2022-10-13 23:56:34 +0300

    context yapısına devam edildi

--- meals_app/src/components/Meals.jsx
+++ meals_app/src/components/Meals.jsx
@@ -1,5 +1,10 @@
+import { useGlobalContext } from "../context"
+
 
 const Meals = () => {
+
+  const context = useGlobalContext()
+  console.log(context)
   return (
     <h1>Meals</h1>
   )
--- meals_app/src/context.jsx
+++ meals_app/src/context.jsx
@@ -4,9 +4,15 @@ import React, {useContext} from "react";
 const AppContext = React.createContext();
 
 const AppProvider = ({children}) => {
-    return <AppContext.Provider value="hello">
+
+
+    return <AppContext.Provider value={{name:"suheyl", role: "student"}}>
         {children}
     </AppContext.Provider>
+}
+
+export const useGlobalContext = () => {
+    return useContext(AppContext)
 }
 
 export {AppContext, AppProvider}
