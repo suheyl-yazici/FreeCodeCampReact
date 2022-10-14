c82364f3e9e9fcd5f75ad11c18754274cdd78673
suheyl-yazici <h.suheylyazici@gmail.com>
2022-10-14 18:00:31 +0300

    axios eklendi

--- meals_app/src/context.jsx
+++ meals_app/src/context.jsx
@@ -1,18 +1,36 @@
-import React, {useContext} from "react";
-
+import React, { useContext, useEffect } from "react";
+import axios from "axios";
 
 const AppContext = React.createContext();
 
-const AppProvider = ({children}) => {
+const AppProvider = ({ children }) => {
 
+  const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s='
+  const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php'
 
-    return <AppContext.Provider value={{name:"suheyl", role: "student"}}>
-        {children}
+  const fetchMeals = async (url) => {
+    try {
+      const {data} = await axios(url);
+      
+      console.log(data);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
+  useEffect(() => {
+    fetchMeals(allMealsUrl);
+  }, []);
+
+  return (
+    <AppContext.Provider value={{ name: "suheyl", role: "student" }}>
+      {children}
     </AppContext.Provider>
-}
+  );
+};
 
 export const useGlobalContext = () => {
-    return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
 
-export {AppContext, AppProvider}
+export { AppContext, AppProvider };
