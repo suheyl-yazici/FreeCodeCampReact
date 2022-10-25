0b09828b2979c211981df89a0231cc46eded55fd
suheyl-yazici <h.suheylyazici@gmail.com>
2022-10-25 14:52:38 +0300

    search component tamamlandı

--- meals_app/src/context.jsx
+++ meals_app/src/context.jsx
@@ -1,38 +1,49 @@
-import React, { useContext, useEffect,useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import axios from "axios";
 
 const AppContext = React.createContext();
 
-const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=a'
-const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php'
-
+const allMealsUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+const randomMealUrl = "https://www.themealdb.com/api/json/v1/1/random.php";
 
 const AppProvider = ({ children }) => {
   const [meals, setMeals] = useState([]);
-const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const fetchMeals = async (url) => {
     setLoading(true);
     try {
-      const {data} = await axios(url);
-      if(data.meals){
+      const { data } = await axios(url);
+      if (data.meals) {
         setMeals(data.meals);
       } else {
-        setMeals([])
+        setMeals([]);
       }
-      
     } catch (error) {
       console.log(error.response);
     }
     setLoading(false);
   };
 
+  const fetchRandomMeal = () => {
+    fetchMeals(randomMealUrl);
+  };
+
   useEffect(() => {
     fetchMeals(allMealsUrl);
   }, []);
 
+
+  useEffect(() => {
+    if(!searchTerm) return
+    fetchMeals(`${allMealsUrl}${searchTerm}`);
+  }, [searchTerm]);
+
   return (
-    <AppContext.Provider value={{ meals, loading }}>
+    <AppContext.Provider
+      value={{ meals, loading, setSearchTerm, fetchRandomMeal }}
+    >
       {children}
     </AppContext.Provider>
   );
