5c8affd405b1f505875fa0e4f2ad3c94727f7b7b
suheyl-yazici <h.suheylyazici@gmail.com>
2022-11-07 13:13:11 +0300

    hata giderildi

--- src/App.js
+++ src/App.js
@@ -136,7 +136,7 @@ function App() {
       />
       <Routes>
         <Route
-          path="/"
+          path="/home"
           element={
             <Employees
               employees={employees}
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -5,7 +5,7 @@ const Nav = () => {
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-                <Link className="nav-link" to ="/">Home</Link>
+                <Link className="nav-link" to ="/home">Home</Link>
             </li>
             <li className="nav-item">
                 <Link className="nav-link" to ="/GroupedTeamMembers">Teams</Link>
